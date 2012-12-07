eb764c87f355a30a3d76ef8e33e0957644db98f5
Connor Collins <connor.collins@gmail.com>
2012-12-07 06:44:29 -0500

    Removed all the unnecessary printing from euler18 and now all it prints is a simple output of what the largest total is.

--- euler18.py
+++ euler18.py
@@ -1,6 +1,4 @@
 # Variable declarations
-numOfRows = 0       # For keeping track of stats
-maxValue = 0        # To help calculating larger sets
 pyramid = []        # To contain the row[]s of numbers
 
 # Function declarations
@@ -18,35 +16,30 @@ def formatNums(item):
 with open("euler18_test_lg.txt") as txt:
     for line in txt:        # Open file and split it by spaces
         line_nums = line.split(" ")
-        numOfRows += 1
         row = [] 
         
         for item in line_nums:
             # retrieve the value of each number
             fNumber = formatNums(item)
-            # check if it's the biggest number ever
-            maxValue = fNumber if fNumber > maxValue else maxValue
             # and store each number in a row[]
             if fNumber > 0:
                 row.append(fNumber)
-            else:       # until you hit an end of line, then onto the next row
-                numOfRows -=1
         if (row != []):
             # then save that row to the pyramid
             pyramid.append(row)
                 
-print "\n\nmaxValue is: ", maxValue
-print "numOfRows is: ", numOfRows
     
 rPyramid = []   # flip the triangle
 for row in reversed(pyramid):
     rPyramid.append(row)
 
-for row in rPyramid:    
-    print row    
-    
 pyramid = []    # reset the pyramid variable so I can reuse it
 
+
+# Calculate which is the larger of the two variables and add it to the next row
+#| 6 7 8 | X 7 8 | X X X | X X X | X X X |
+#|  4 5  |  4 5  | 11 13 |  X 13 |  X X  |
+#|   3   |   3   |   3   |   3   |   16  |
 for row_index, row in enumerate(rPyramid):
     new_row = []
     for item_index, item in enumerate(row):
@@ -55,14 +48,11 @@ for row_index, row in enumerate(rPyramid):
             addItem = item if item > row[item_index+1] else row[item_index+1]
             new_row.append(addItem + rPyramid[row_index+1][item_index])
         except IndexError:
-            print "D", row_index
             try:
                 rPyramid[row_index+1] = new_row
             except IndexError:
-                print "Done!"
+                pass
             finally:
                 pyramid.append(new_row)
-            
-    
-print pyramid
-print rPyramid
+
+print "Largest Total is:", rPyramid[len(rPyramid)-1][0]
